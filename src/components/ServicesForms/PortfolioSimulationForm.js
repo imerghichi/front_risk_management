f91d687f9ed6b0ba86f6459eb796e162b383a5d7
imerghichi <hypathie14@gmail.com>
2020-06-04 21:55:19 +0100

    More Forms

---
+++ src/components/ServicesForms/PortfolioSimulationForm.js
@@ -0,0 +1,51 @@
+import {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+
+function RejectionSamplerForm({ submitHandler }) {
+    const formInitialState = {
+        number_of_simulations : 3,
+        number_of_years:1,
+
+    };
+
+    const [formState, updateFormState] = useState(formInitialState);
+    const handleUpdate = (fieldKey, newValue) => {
+        const newFormState = { ...formState};
+        switch (fieldKey) {
+            case "number_of_simulations":
+                newFormState.number_of_simulations = newValue;
+                break;
+            case "number_of_years":
+                newFormState.number_of_years = newValue;
+                break;
+            default:
+                break;
+        }
+        updateFormState(newFormState);
+    };
+
+    return (
+        <Form noValidate validated={true}>
+            <Form.Row>
+                <Form.Group as = {Col} md="4">
+                    <Form.Label>Entrer le nombre de simulations:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        onBlur = {(e)=>handleUpdate("number_of_simulations",e.target.value)}
+                    />
+                </Form.Group>
+            </Form.Row>
+            <Form.Row>
+                <Form.Group as = {Col} md="4">
+                    <Form.Label>Entrer le nombre des années:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        onBlur = {(e)=>handleUpdate("number_of_years",e.target.value)}
+                    />
+                </Form.Group>
+            </Form.Row>
+        </Form>
+    );
+}
+export default FaultTreeForm;
---
+++ src/components/ServicesForms/RejectionSamplerForm.js
@@ -0,0 +1,38 @@
+import {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+
+function RejectionSamplerForm({ submitHandler }) {
+    const formInitialState = {
+        threshold : 0.0,
+    };
+
+    const [formState, updateFormState] = useState(formInitialState);
+    const handleUpdate = (fieldKey, newValue) => {
+        const newFormState = { ...formState};
+        switch (fieldKey) {
+            case "threshold":
+                newFormState.threshold = newValue;
+                break;
+            default:
+                break;
+        }
+        updateFormState(newFormState);
+    };
+
+    return (
+        <Form noValidate validated={true}>
+            <Form.Row>
+                <Form.Group as = {Col} md="4">
+                    <Form.Label>Probabilité:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        step="0.01"
+                        onBlur = {(e)=>handleUpdate("threshold",e.target.value)}
+                    />
+                </Form.Group>
+            </Form.Row>
+        </Form>
+    );
+}
+export default FaultTreeForm;
