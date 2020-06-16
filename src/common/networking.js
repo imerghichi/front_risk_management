d1ebb9ee022b3b5c859ffbf49825093c36352222
imerghichi <hypathie14@gmail.com>
2020-06-16 02:58:47 +0100

    networking problem

--- src/common/networking.js
+++ src/common/networking.js
@@ -1,7 +1,17 @@
 const API_PATH = "http://localhost:8080";
 
 async function fetchApi(path, options = {}) {
-    const data = await fetch(API_PATH + path, options);
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    const params = {
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        ...options
+    };
+
+    const data = await fetch(API_PATH + path, params);
     return await data.json();
 }
 
